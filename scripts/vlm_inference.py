ac41b40598e9d17ad7f0f3bd2693b7404b61b65c
HaccerKat <jiapengbai@gmail.com>
2026-03-05 20:56:27 -0500

    added default settings and added comments

--- scripts/vlm_inference.py
+++ scripts/vlm_inference.py
@@ -1,16 +1,24 @@
 from google import genai
 import time
 import argparse
+from dotenv import load_dotenv
+import os
 
-def main():
+# This script demonstrates how to use the Google Gemini API for video understanding tasks.
+# Put videos in vlm-pref/demos
+# SPECIFICATIONS:
+# Run the script in the vlm-pref folder
+# If you want a default API key, please create a .env file in the vlm-pref/scripts folder and add your key as GOOGLE_API_KEY=your_api_key
+
+def main(api_key):
     parser = argparse.ArgumentParser(description="Google AI inference")
 
     # Add args
-    parser.add_argument("--api_key", type=str, default="",
+    parser.add_argument("--api_key", type=str, default=api_key,
                         help="put your api key")
-    parser.add_argument("--model", type=str, default="",
+    parser.add_argument("--model", type=str, default="gemini-3-flash-preview",
                         help="which model to use (e.g. gemini-3-flash-preview)")
-    parser.add_argument("--video_path", type=str, default="",
+    parser.add_argument("--video_path", type=str, default="demos/bad_shaky_agentview.mp4",
                         help="where your video at")
     parser.add_argument("--prompt", type=str, default=None,
                         help="text prompt to help the vlm (if unspecified, will use prompt in the txt file)")
@@ -21,7 +29,7 @@ def main():
     video_file = client.files.upload(file=args.video_path)
     prompt = args.prompt
     if prompt is None:
-        with open("vlm_prompt.txt", 'r') as f:
+        with open("scripts/vlm_prompt.txt", 'r') as f:
             prompt = f.read()
 
     while video_file.state.name != "ACTIVE":
@@ -39,6 +47,7 @@ def main():
 
     print(response.text)
 
-
 if __name__ == '__main__':
-    main()
+    load_dotenv()
+    api_key = os.getenv("GOOGLE_API_KEY")
+    main(api_key)
